7f7cc7b79af37348a28ec45b79d43166ae7c187e
j8761336 <105021026@live.asia.edu.tw>
2016-11-07 07:16:36 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,21 @@
 package hw;
-
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn =new Scanner(System.in);
+		float a =scn.nextInt();
+		int b =scn.nextInt();
+		System.out.println(fun(a, b));
+	}
+	public static float fun (float x,int y){
+		if(y == 1){
+			x = x*9/5+32;
+		}
+		else if(y == 2){
+			x = (x-32) * (5/9);
+		}
+		return x;
 	}
 
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,20 @@
 package hw;
-
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn =new Scanner(System.in);
+		int a =scn.nextInt();
+		double b =scn.nextInt();
+		System.out.print(fun(a,b));
 	}
-
-}
+	public static double fun(int x,double y){
+		if (x==1){
+			y = ((y-170)*0.6+62); 
+		}
+		else if(x==2){
+			y= ((y-158)*0.5+52);
+		}
+		return y;
+	}}
